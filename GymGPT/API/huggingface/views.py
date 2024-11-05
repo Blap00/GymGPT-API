30b2e5dd4697d91f1e99c2d8c36ccbce444a65a7
Blap00 <fa.palma@duocuc.cl>
2024-11-05 18:58:56 -0300

    UPD MINS & TIMEOUT

--- GymGPT/API/huggingface/views.py
+++ GymGPT/API/huggingface/views.py
@@ -351,7 +351,9 @@ class RequestPasswordResetView(generics.GenericAPIView):
 
             # Generar y enviar el código de recuperación
             recovery_code = get_random_string(length=6, allowed_chars='0123456789')
-            cache.set(f'recovery_code_{user.id}', recovery_code, timeout=3200)
+            timeout_sec=1600
+            cache.set(f'recovery_code_{user.id}', recovery_code, timeout=timeout_sec)
+            timeout_min=timeout_sec//60
 
             # Enviar el correo con el código de recuperación
             subject = "Recuperación de contraseña"
@@ -361,7 +363,7 @@ class RequestPasswordResetView(generics.GenericAPIView):
                 <p>Estimado {user.first_name},</p>
                 <p>Has solicitado recuperar tu contraseña. Utiliza el siguiente código para restablecerla:</p>
                 <h2 style="color: #28a745;">{recovery_code}</h2>
-                <p>Este código es válido por 10 minutos. Si no solicitaste este correo, ignóralo.</p>
+                <p>Este código es válido por {timeout_min} minutos. Si no solicitaste este correo, ignóralo.</p>
             </div>
             """
 
